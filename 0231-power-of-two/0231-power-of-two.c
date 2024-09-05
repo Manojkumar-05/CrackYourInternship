17d4b2e203a4412c3e38bf600fa9b1fdbd186e7f
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-09-05 11:58:42 +0530

    Time: 5 ms (22.36%), Space: 7.5 MB (61.92%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.c
+++ 0231-power-of-two/0231-power-of-two.c
@@ -1,6 +1,6 @@
 bool isPowerOfTwo(int n) {
-    long long it = 4;
-    if(n == 1 || n == 2 || n == 4) return true;
+    long long it = 2;
+    if(n == 1 || n == 2) return true;
     if(n % 2 == 0){
         while(it <= n){
             if(n == it) return true;
